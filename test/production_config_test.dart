35ed6790576388e9f906c5f8f144ad9ef9036bc4
znak99 <xhahekcldhkznak0113@gmail.com>
2026-07-31 02:49:13 +0900

    style: format production config test

--- test/production_config_test.dart
+++ test/production_config_test.dart
@@ -11,10 +11,7 @@ void main() {
       expect(ApiConfig.baseUrl, 'https://kisou.znak99.cloud');
       expect(ApiConfig.developmentFeaturesEnabled, isFalse);
       expect(ApiConfig.showDevelopmentLogin, isFalse);
-      expect(
-        ApiConfig.secureStorageService,
-        'flutter_secure_storage_service',
-      );
+      expect(ApiConfig.secureStorageService, 'flutter_secure_storage_service');
     },
     skip: ApiConfig.environment != 'production',
   );
